cbbc5d2c24b1e8b31005b98beb61f415dde8eadc
Mayank <you@example.com>
2020-10-31 22:27:21 +0530

    post load

--- ior_research/IOTClient.py
+++ ior_research/IOTClient.py
@@ -3,6 +3,8 @@ import time
 import json
 import socket
 import os
+import logging
+
 
 class IOTClient(threading.Thread):
     """Class used to access IOR Server"""
@@ -19,6 +21,8 @@ class IOTClient(threading.Thread):
         """
 
         threading.Thread.__init__(self)
+        logging.basicConfig(format=f'%(asctime)s - {token}-{code} %(message)s', level=logging.INFO)
+
         self.__code = code
         self.__token = token
         self.__to = to
@@ -33,24 +37,39 @@ class IOTClient(threading.Thread):
         self.__server = server
         self.isTunneled = isTunneled
         self.connected = False
+        self.closed = False
+        self.__s = None
 
-        self._writeline("*" * 80)
-        self._writeline("Using Beta - Version: %s" % self.version())
-        self._writeline("Server Configuration IP: %s" % (self.__server))
-        self._writeline("User Token %s" % self.__token)
-        self._writeline("From Code: %d    To Code: %d" % (self.__code, self.__to))
-        self._writeline("Time Delay(in Seconds): %d" % self.__time_delay)
-        self._writeline("Tunneling Enabled: " + str(self.isTunneled))
-        self._writeline("*" * 80)
+        logging.info("*" * 80)
+        logging.info("Using Beta - Version: %s" % self.version())
+        logging.info("Server Configuration IP: %s" % (self.__server))
+        logging.info("User Token %s" % self.__token)
+        logging.info("From Code: %d    To Code: %d" % (self.__code, self.__to))
+        logging.info("Time Delay(in Seconds): %d" % self.__time_delay)
+        logging.info("Tunneling Enabled: " + str(self.isTunneled))
+        logging.info("*" * 80)
 
         if not os.path.exists('./logs') and save_logs == True:
             os.mkdir('./logs')
 
         if(not self.reconnect()):
             raise Exception("Could not connect to Server at %s:%d-%d"%(self.__server,self.__httpPort,self.__port))
+        self.setName("Reader-%s-%d"%(self.__token,self.__to))
+        if not self.isTunneled:
+            self.heartBeat = threading.Thread(target=IOTClient.__sendThread,args=(self,))
+            self.heartBeat.setName("Heartbeat-%s-%d"%(self.__token,self.__code))
+            self.heartBeat.start()
+
+
+
     @staticmethod
-    def createRevertedClients():
-        pass
+    def createRevertedClients(token,code,to,server="localhost",httpPort=8080,tcpPort=8000):
+        client1 = IOTClient(token=token, debug=True, code=code, to=to, server=server, httpPort=httpPort,
+                            tcpPort=tcpPort)
+        client2 = IOTClient(token=token, debug=True, code=to, to=code, server=server, httpPort=httpPort,
+                            tcpPort=tcpPort)
+
+        return (client1,client2)
     @staticmethod
     def version():
         return "v0.3.7"
@@ -61,29 +80,29 @@ class IOTClient(threading.Thread):
 
     def reconnect(self):
         import requests
-        r = requests.post('http://%s:%d/subscribe/%s/%d/%d' % (self.__server,self.__httpPort, self.__token, self.__code,self.__to))
+        r = requests.post('http://%s:%d/subscribe?uuid=%s&from=%d&to=%d' % (self.__server,self.__httpPort, self.__token, self.__code,self.__to))
         if r.status_code == 404:
-            self._writeline("Request Failed")
+            logging.info("Request Failed")
             return False;
         if r.status_code == 409:
             raise Exception("Conflict while connecting, may another device is pre connected to the server")
         if r.status_code != 201:
             raise Exception("Invalid Credentials")
 
-        print("Request Successfully made to Server")
+        logging.info("Request Successfully made to Server")
         s = r.content
-        print(s)
+        logging.info(s)
+
+        if(self.__s is not None):
+            self.__s.close()
+            self.__s = None
 
         self.__s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.__s.connect((self.__server, self.__port))
         self.__s.sendall(s);
 
         self.file = self.__s.makefile('rw')
-        self._writeline("Connected to Socket Server")
-
-        if not self.isTunneled:
-            self.heartBeat = threading.Thread(target=IOTClient.__sendThread,args=(self,))
-            self.heartBeat.start()
+        logging.info("Connected to Socket Server")
 
         self.connected = True
         return True
@@ -97,6 +116,13 @@ class IOTClient(threading.Thread):
             try:
                 self.__lock.acquire()
                 self.getSocket().send(b"\n")
+            except  AttributeError:
+                if( self.closed):
+                    logging.warning("Client already closed, Skipping update")
+                    break;
+            except ConnectionAbortedError as cae:
+                self.connected = False
+                logging.error("Connection Aborted",exc_info=True)
             finally:
                 self.__lock.release()
             time.sleep(self.__time_delay)
@@ -104,23 +130,27 @@ class IOTClient(threading.Thread):
     def set_on_receive(self,fn):
         self.on_receive = fn
 
-    def _writeline(self,msg):
-        if self.debug:
-            print(msg)
-
     def __send(self,msg):
+        if(self.connected == False):
+            logging.error("Server not connected Skipping")
+            return False
         try:
             data = json.dumps(msg)
-            print(data)
             self.__lock.acquire()
             self.__s.send(data.encode() + b'\r\n')
+        except ConnectionAbortedError as cae:
+            self.connected = False
+            logging.error(cae)
         finally:
             self.__lock.release()
 
 
-    def sendMessage(self,message,metadata = None):
+    def sendMessage(self,message,metadata = None,status = None):
         msg = dict()
         msg["message"] = message
+        if(status is not None):
+            msg["status"] = status
+
         if metadata is not None:
             msg["syncData"] = metadata
 
@@ -128,11 +158,12 @@ class IOTClient(threading.Thread):
 
     def close(self):
         self.connected = False
+        self.closed = True
 
         self.__s.close()
         self.file.close()
 
-        self._writeline("Socket Closed")
+        logging.info("Socket Closed")
         if self.__on_close != None:
             self.__on_close()
 
@@ -146,48 +177,113 @@ class IOTClient(threading.Thread):
         return data
 
     def run(self):
-        self._writeline("Starting Thread")
-        while self.connected:
+        logging.info("Starting Thread")
+        while not self.closed:
+            if not self.connected:
+                time.sleep(1)
+                continue
             try:
                 msg = self.readData()
                 if msg is not None:
                     try:
                         self.on_receive(msg)
                     except Exception as ex:
-                        self._writeline("Error Occured while invoking Receive Function")
-                        self._writeline(ex)
+                        logging.info("Error Occured while invoking Receive Function")
+                        logging.info(ex)
             except socket.timeout:
-                print("socket timeout")
+                logging.info("socket timeout")
             except Exception as cae:
-                print("Error Occured!!!")
-                print(cae)
+                self.connected = False
+                logging.error("Error Occured!!!")
+                logging.error(cae)
                 break;
             time.sleep(0.01)
-        print("Thread Terminated")
-        self.close()
+        logging.info("Thread Terminated")
 
-token = "5a5a83c3-2588-42fb-84bd-fa3129a2ac45"
-code = 1234
-to = 789
+class IOTClientWrapper(threading.Thread):
+    def __init__(self,token,code,to,config: dict = None):
+        threading.Thread.__init__(self)
+        self.config = {
+            "server": "localhost",
+            "httpPort": 8080,
+            "tcpPort": 8000,
+            "token": token,
+            "code": code,
+            "to": to
+        }
+        if config is not None:
+            for key,value in config.items():
+                self.config[key] = value
 
-counter = 0
+        self.closed = False
+        self.set_on_receive(None)
+        self.client = self.recreateClient()
+
+    def set_on_receive(self,fn):
+        self.fn = fn
+
+    def terminate(self):
+        self.closed = True
+        if self.client is not None:
+            self.client.close()
+
+    def sendMessage(self,**data):
+        try:
+            return self.client.sendMessage(**data)
+        except Exception:
+            return False
+
+    def recreateClient(self):
+        return IOTClient(**self.config)
+
+    def run(self):
+        while not self.closed:
+            try:
+                if self.client is None:
+                    self.client = self.recreateClient()
+                self.client.set_on_receive(self.fn)
+                self.client.start()
+                self.client.join()
+                self.client.close()
+                print("Watcher Thread Closed")
+                del self.client
+                self.client = None
+            except Exception:
+                logging.error("Watcher Error: ",exc_info=True)
+
+
 def on_receive(x):
     global counter
-    print(counter,"Received",x)
-    counter += 1
+    print("Received",x)
 
 if __name__ == "__main__":
-    client1 = IOTClient(token=token,debug=True,code=code,to=to,server="192.168.46.3")
-    client2 = IOTClient(token=token, debug=True, code=to, to=code, server="192.168.46.3")
+    config = {
+        "server": "localhost",
+        "httpPort": 5001,
+        "tcpPort": 8000
+    }
+    token = "5a5a83c3-2588-42fb-84bd-fa3129a2ac45"
+    code = 1234
+    to = 789
+
+    client1 = IOTClientWrapper(token,code,to,config=config)
+    client2 = IOTClientWrapper(token,to,code,config = config);
+    client2.set_on_receive(on_receive)
+
     client1.start()
     client2.start()
-    client2.set_on_receive(on_receive)
-    for i in range(10):
-        print(i)
-        client1.sendMessage("Hello from Client")
-        time.sleep(0.01)
+
+    while True:
+        print("Sending Message")
+        client1.sendMessage(message = "Hello from Client")
+        time.sleep(1)
+
     print()
     print(counter)
     time.sleep(5000)
+
+    client1.join()
+    client2.join()
+
     client1.close()
     client2.close()
---
+++ ior_research/drone/ior_drone.py
@@ -0,0 +1,115 @@
+import time, math
+from dronekit import LocationGlobal, LocationGlobalRelative
+from pymavlink import mavutil
+
+class Drone:
+    def __init__(self, copter):
+        self.copter = copter
+        self.targetAltitude = self.copter.location.global_relative_frame.alt
+        self.changeMode('GUIDED')
+
+    def setTargetAltitude(self,altitude):
+        self.targetAltitude = altitude
+
+    def moveForward(self,distance=0,altitude=None):
+        if altitude is not None:
+            self.setTargetAltitude(altitude)
+
+    def changeMode(self,mode):
+        self.copter.mode = mode
+        while self.copter.mode != mode:
+            time.sleep(0.5)
+
+    def takeoff(self,altitude = 1):
+        self.setTargetAltitude(altitude)
+        self.copter.simple_takeoff(self.targetAltitude)
+        while self.copter.mode.name == 'GUIDED':
+            print(" Altitude: ", self.copter.location.global_relative_frame)
+            # Break and return from function just below target altitude.
+            if self.copter.location.global_relative_frame.alt is None:
+                continue;
+            if self.copter.location.global_relative_frame.alt >= self.targetAltitude * 0.95:
+                print("Reached target altitude")
+                break
+            time.sleep(1)
+
+    def setHeading(self,heading):
+        while self.copter.mode.name == "GUIDED" and abs(heading - self.copter.heading) > 5:
+            print(heading - self.copter.heading)
+            msg = self.copter.message_factory.command_long_encode(
+                0, 0,  # target system, target component
+                mavutil.mavlink.MAV_CMD_CONDITION_YAW,  # command
+                0,  # confirmation
+                heading,  # param 1, yaw in degrees
+                0,  # param 2, yaw speed deg/s
+                1 if (heading - self.copter.heading) < 0 else 0,  # param 3, direction -1 ccw, 1 cw
+                0,  # param 4, relative offset 1, absolute angle 0
+                0, 0, 0)  # param 5 ~ 7 not used
+            self.copter.send_mavlink(msg)
+
+            msg = self.copter.message_factory.set_position_target_local_ned_encode(
+                0,  # time_boot_ms (not used)
+                0, 0,  # target_system, target_component
+                mavutil.mavlink.MAV_FRAME_BODY_NED,  # frame
+                0b0000111111000111,  # type_mask (only speeds enabled)
+                0, 0, 0,  # x, y, z positions
+                0, 0, 0,  # x, y, z velocity in m/s
+                0, 0, 0,  # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
+                0, 0)  # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
+            # send command to vehicle
+            self.copter.send_mavlink(msg)
+
+            time.sleep(0.1)
+
+def createCircle(center,radius,yaw=0):
+    points = []
+
+    target_location = get_location_from_distance(yaw,center,radius)
+    points.append(target_location)
+    c_yaw = 0
+    while c_yaw < 360:
+        target_location = get_location_from_distance(c_yaw, center, radius)
+        points.append(target_location)
+        c_yaw += 30
+
+    return points
+
+
+def get_location_from_distance(yaw,current_location,target_distance):
+    rad = math.radians(yaw)
+    dNorth =  math.cos(rad) * target_distance
+    dEast = math.sin(rad) * target_distance
+    return get_location_metres(current_location,dNorth,dEast)
+
+def get_location_metres(original_location, dNorth, dEast):
+    earth_radius = 6378137.0  # Radius of "spherical" earth
+    # Coordinate offsets in radians
+    dLat = dNorth / earth_radius
+    dLon = dEast / (earth_radius * math.cos(math.pi * original_location.lat / 180))
+
+    # New position in decimal degrees
+    newlat = original_location.lat + (dLat * 180 / math.pi)
+    newlon = original_location.lon + (dLon * 180 / math.pi)
+    if type(original_location) is LocationGlobal:
+        targetlocation = LocationGlobal(newlat, newlon, original_location.alt)
+    elif type(original_location) is LocationGlobalRelative:
+        targetlocation = LocationGlobalRelative(newlat, newlon, original_location.alt)
+    else:
+        raise Exception("Invalid Location object passed")
+
+    return targetlocation;
+
+
+def get_distance_metres(aLocation1, aLocation2):
+    dlat = aLocation2.lat - aLocation1.lat
+    dlong = aLocation2.lon - aLocation1.lon
+    return math.sqrt((dlat * dlat) + (dlong * dlong)) * 1.113195e5
+
+
+def get_bearing(aLocation1, aLocation2):
+    off_x = aLocation2.lon - aLocation1.lon
+    off_y = aLocation2.lat - aLocation1.lat
+    bearing = 90.00 + math.atan2(-off_y, off_x) * 57.2957795
+    if bearing < 0:
+        bearing += 360.00
+    return bearing;
