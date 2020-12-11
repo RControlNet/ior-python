2b0590103bfc41cbe24f422d0ee669890b02af3f
Mayank <mayank31313@gmail.com>
2020-12-12 01:33:00 +0530

    proxyclient ++  video

--- examples/IORProxyClient.py
+++ examples/IORProxyClient.py
@@ -3,6 +3,7 @@ import sys, os
 sys.path.append(os.getcwd())
 from ior_research.utils.proxy import ProxyClient
 from ior_research.IOTClient import IOTClientWrapper
+from ior_research.utils.video import VideoTransmitter, createVideoTransmitter
 import json
 
 config = {
@@ -14,17 +15,26 @@ config = {
 token = "a9b08f66-8e6f-4558-b251-da7163aac420"
 code = 789
 to = 1234
+videoTransmitter = None
 
 def on_receive(msg):
+    global videoTransmitter
+    if(msg['message'] is not None and msg['message'] == "START_STREAMER"):
+        videoTransmitter = createVideoTransmitter()
+        videoTransmitter.openBrowserAndHitLink("mayank31313@gmail.com", "12345678")
+    if(videoTransmitter is not None and msg['message'] is not None and msg['message'] == "STOP_STREAMER"):
+        videoTransmitter.quit()
+
     msg = json.dumps(msg)
-    proxyClient.sendData(msg.encode())
+    print(msg)
+#    proxyClient.sendData(msg.encode())
 
 def on_send(msg):
     print("Send",msg)
 
-proxyClient = ProxyClient()
-proxyClient.connect(callback=on_send, server=("10.42.1.95", 54753))
-proxyClient.start()
+#proxyClient = ProxyClient()
+# proxyClient.connect(callback=on_send, server=("10.42.1.95", 54753))
+# proxyClient.start()
 
 t2 = IOTClientWrapper(token,code,to,config=config)
 
--- ior_research/utils/video.py
+++ ior_research/utils/video.py
@@ -5,43 +5,39 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 class VideoTransmitter:
-    def __init__(self, uniqueIdentifier, driver, server="https://192.168.0.131:8003/#/"):
-        self.identifier = uniqueIdentifier
+    def __init__(self, driver, server="https://192.168.0.131:8003/#/"):
         self.server = server
         self.driver = driver
 
-    def openBrowserAndHitLink(self):
-        
+    def openBrowserAndHitLink(self, username, password):        
         self.driver.get("https://192.168.0.131:8080/")
         print(self.driver.title)
         if(self.driver.title == "Privacy error"):
-            print("Privacy Error")
             self.driver.find_element_by_id('details-button').click()
             self.driver.find_element_by_id('proceed-link').click()
 
         self.driver.get(self.server)
         print(self.driver.title)
         if(self.driver.title == "Privacy error"):
-            print("Privacy Error")
             self.driver.find_element_by_id('details-button').click()
             self.driver.find_element_by_id('proceed-link').click()
-        self.driver.find_element_by_name('username').send_keys("mayank31313@gmail.com")
-        self.driver.find_element_by_name('password').send_keys("12345678")
+        self.driver.find_element_by_name('username').send_keys(username)
+        self.driver.find_element_by_name('password').send_keys(password)
         self.driver.find_element_by_id('submit').click()
 
     def close(self):
         self.driver.quit()
 
-def createVideoTransmitter(identifier):
+def createVideoTransmitter():
     options = Options()
     options.add_experimental_option("prefs", { \
         "profile.default_content_setting_values.media_stream_mic": 1,     # 1:allow, 2:block 
         "profile.default_content_setting_values.media_stream_camera": 1,  # 1:allow, 2:block
     })
-    return VideoTransmitter(identifier, webdriver.Chrome(chrome_options=options))
+    return VideoTransmitter(webdriver.Chrome(chrome_options=options))
 
 if __name__ == "__main__":
-    transmitter = createVideoTransmitter("hello")
-    transmitter.openBrowserAndHitLink()
+    transmitter = createVideoTransmitter()
+    transmitter.openBrowserAndHitLink("mayank31313@gmail.com", "12345678")
     input()
     transmitter.close()
