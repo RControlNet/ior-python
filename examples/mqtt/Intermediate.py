04fa0c99ac2b379e57dd35006614d46c604154ca
Mayank <mayank31313@gmail.com>
2021-04-11 20:10:47 +0530

    video config added

--- examples/mqtt/Intermediate.py
+++ examples/mqtt/Intermediate.py
@@ -1,7 +1,13 @@
 from ior_research.mqtt import Communicator
 from ior_research.IOTClient import IOTClientWrapper
+from ior_research.drone.ior_drone import DroneOperations, map_name_drone_attribute
+from ior_research.utils.video import createVideoTransmitter
+import os
+
 import argparse
+
 if __name__ == "__main__":
+    os.environ['RCONTROLNET'] = "../../config/iorConfigs.config"
     parser = argparse.ArgumentParser()
     parser.add_argument("--server", default="localhost", type=str)
     parser.add_argument("--broker", default="localhost", type=str)
@@ -19,12 +25,25 @@ if __name__ == "__main__":
     configFrom = config.copy()
     configFrom['file'] = "../../config/to.json"
     token = "default"
+    videoTransmitter = None
 
     def on_receive(msg):
-        print(msg)
+        global videoTransmitter
         message = msg['message']
-        for (key,value) in msg['syncData'].items():
-            client.sendMessage(sm=key + "/" +str(value))
+        if map_name_drone_attribute(message,DroneOperations.START_STREAMER):
+            if videoTransmitter is None:
+                videoTransmitter = createVideoTransmitter()
+
+            if not videoTransmitter.checkBrowserAlive():
+                videoTransmitter.openBrowserAndHitLink()
+            else:
+                videoTransmitter.close()
+                videoTransmitter = createVideoTransmitter()
+                videoTransmitter.openBrowserAndHitLink()
+
+        else:
+            for (key,value) in msg['syncData'].items():
+                client.sendMessage(sm=key + "/" +str(value))
 
 
     t1 = IOTClientWrapper(token=token ,config=configFrom)
