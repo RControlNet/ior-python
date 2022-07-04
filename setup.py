3e9a425db6376e4da10a3f3a8485aa4aaabe69be
Mayank <mayank31313@gmail.com>
2022-07-05 00:14:59 +0530

    RCNT-feature/pypi

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+with open("requires.txt", "r") as stream:
+    requirements = list(map(lambda dep: str(dep).strip(), stream.read().split("\n")))
 
 setuptools.setup(
      name='ior_research',
-     version='v2.0.0',
+     version='v2.0.1',
      author="Mayank Shinde",
      author_email="mayank31313@gmail.com",
      description="A platform to control robots and electronic device over Internet",
@@ -14,6 +16,7 @@ setuptools.setup(
      url="https://github.com/mayank31313/ior-python",
      packages=setuptools.find_packages(),
      # packages=['ior_research'],
+     install_requires = requirements,
      keywords=['ior','iot','network_robos', 'control_net'],
      classifiers=[
          "Programming Language :: Python :: 3",
